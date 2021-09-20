e5c4275b8f6ac082dccd6e971023dfbc4411daaa
Kenni <kenni.bawden@gmail.com>
2021-09-20 06:43:43 +0000

    Added another type of round

--- TODO.md
+++ TODO.md
@@ -1,5 +1,9 @@
 # TODO
 
-* Add disambiguation handling
-* Add context for words in selected article
+* Better word prompts
 * Turn into a website
+* Fix pluralisation
+* Other kinds of prompts
+* Don't allow answers that contain words in the prompt
+* Allow close words?
+* Disambiugation pages in disambiguation results
--- main.py
+++ main.py
@@ -2,29 +2,95 @@ import wikipedia
 import re
 import random
 
-words = open('words.txt').readlines()
-word = random.choice(words).strip()
-print(word)
+def get_random_article():
+  article = get_article(wikipedia.random())
+  print(article.title)
+  print(article.summary[:250])
+  return article
 
-def guess_article(word):
-  article_title = input(f'Find the page with the most {word}: ')
+def get_random_word():
+  words = open('words.txt').readlines()
+  word = random.choice(words).strip()
+  print(word)
+  return word
 
+def get_article(article_title):
   search = wikipedia.search(article_title)
-  article = wikipedia.page(search[0], auto_suggest=False)
+  try:
+    article = wikipedia.page(search[0], auto_suggest=False)
+  except wikipedia.exceptions.DisambiguationError as e:
+    for i, option in enumerate(e.options):
+      print(f'{i}) {option}')
+    choice = int(input('Choose a page: '))
+    article = wikipedia.page(e.options[choice], auto_suggest=False)
+  return article
 
-  matches = re.findall(word, article.content, re.IGNORECASE)
+def guess_article_with_most_words(word):
+  article_title = get_answer(f'Find the page with the most {word}: ', [word])
+  article = get_article(article_title)
 
-  print(f'The {article.title} article has {len(matches)} {word}s')
+  words =  re.sub('[^\w\d\s]', '', article.content).split()
+  matches = words.count(word)
 
-  return len(matches)
+  print(f'The {article.title} article has {matches} {word}s')
 
+  if matches:
+    print(context(word, article.content))
 
-player1 = guess_article(word)
-player2 = guess_article(word)
+  return matches
 
-if player1 > player2:
-  print('Player 1 wins!')
-elif player1 < player2:
-  print('Player 2 wins!')
-else:
-  print('It\'s a draw!')
+def guess_article_with_most_common_links(article):
+  article_title = get_answer(f'Find the page with the common links with {article.title}: ', article.title.split())
+  guessed_article = get_article(article_title)
+
+  common_links = set(article.links) & set(guessed_article.links)
+
+  print(f'The {guessed_article.title} article has {len(common_links)} links in common with {article.title}')
+  print(common_links)
+
+  return len(common_links)
+
+def get_answer(prompt, invalid_inputs=None):
+  invalid_inputs = invalid_inputs or []
+  answer = None
+
+  while not answer:
+    answer = input(prompt)
+    if set(answer.split()) & set(invalid_inputs):
+      print(f'You can\'t use {answer}')
+      answer = None
+  
+  return answer
+
+def context(word, article):
+  for sentence in re.split('\n|\.', article):
+    words = re.sub('[^\w\d\s]', '', sentence).split()
+    if word in words:
+      return sentence
+
+def round(setup, ask):
+  context = setup()
+  player1 = ask(context)
+  player2 = ask(context)
+
+  while player1 == 0 and player2 == 0:
+    retry = input('Retry? Y/N: ').lower()
+    if retry == 'y':
+      player1 = ask(context)
+      player2 = ask(context)
+    else:
+      break
+
+  if player1 > player2:
+    print('Player 1 wins!')
+  elif player1 < player2:
+    print('Player 2 wins!')
+  else:
+    print('It\'s a draw!')
+
+if __name__ == '__main__':
+  print('Round 1')
+  round(get_random_word, guess_article_with_most_words)
+  print()
+  print('Round 2')
+  round(get_random_article, guess_article_with_most_common_links)
