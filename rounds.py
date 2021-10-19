1e184ff7efdabaf98d189dd67027ecbb1ab7d7ed
Kenni <kenni.bawden@gmail.com>
2021-10-19 11:14:39 +0000

    Added a most frequent word round

--- rounds.py
+++ rounds.py
@@ -9,14 +9,16 @@ class Round():
     raise Exception('Not implemented')
 
 
-class MostWordsRound(Round):
+class HighestWordCountRound(Round):
   def __init__(self):
     self.word = wiki.get_random_word()
     self.question = f'Which page has the most {self.word}?'
     self.data = {}
 
   def score(self, answer):
-    return wiki.get_article_wordcount(answer, self.word)
+    article = wiki.get_article(answer)
+    score = wiki.get_article_wordcount(article, self.word)
+    return article.title, score
 
 class MostCommonLinksRound(Round):
   def __init__(self):
@@ -69,4 +71,25 @@ class ImageRound(Round):
       score = 0
     return article.title, score
 
-ROUNDS = [MostWordsRound, MostCommonLinksRound, ImageRound, MostViewsRound]
+class MostFrequentWordRound(Round):
+  def __init__(self):
+    self.article_title = ''
+    self.question = ''
+    self.data = {}
+  
+  def setup(self, article_title):
+    self.article_title = article_title
+    self.article = wiki.get_article(self.article_title)
+    self.question = f'Guess the which word appears the most in {self.article_title}'
+
+  def score(self, answer):
+    score = wiki.get_article_wordcount(self.article, answer)
+    return self.article.title, score
+
+ROUNDS = [
+  HighestWordCountRound,
+  MostCommonLinksRound,
+  ImageRound,
+  MostFrequentWordRound,
+  MostViewsRound
+]
