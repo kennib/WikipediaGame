c963b1bbf5bea983d4d1880827ec2b8877573cee
Caitlin Mangan <caitiem42@gmail.com>
2021-10-20 03:38:07 +0000

    Round-specific score types, example sentences for HighestWordCountRound

--- rounds.py
+++ rounds.py
@@ -13,22 +13,33 @@ class HighestWordCountRound(Round):
   def __init__(self):
     self.word = wiki.get_random_word()
     self.question = f'Which page has the most {self.word}?'
-    self.data = {}
+    self.data = {
+      'answer': {
+        'scoreType': 'Word count',
+      }
+    }
 
   def score(self, answer):
     article = wiki.get_article(answer)
     score = wiki.get_article_wordcount(article, self.word)
+    if score:
+      context = wiki.context(answer, article)
+      self.data['answer']['example'] = context if context else 'Failed to find example sentence.'
     return article.title, score
 
 class MostCommonLinksRound(Round):
   def __init__(self):
     self.article_title = ''
     self.question = ''
-    self.data = {}
+    self.data = {
+      'answer': {
+        'scoreType': 'Link count'
+      }
+    }
 
   def setup(self, article_title):
     self.article_title = article_title
-    self.question = f'Find the page with the common links with {self.article_title}'
+    self.question = f'Find the page with the most common links with {self.article_title}'
 
   def score(self, answer):
     return wiki.get_common_links(self.article_title, answer)
@@ -38,7 +49,11 @@ class MostViewsRound(Round):
     self.year, self.month = wiki.get_random_month()
     self.word = wiki.get_random_word()
     self.question = f'Find the most popular article for {self.month}/{self.year} which contains {self.word}'
-    self.data = {}
+    self.data = {
+      'answer': {
+        'scoreType': 'View count'
+      }
+    }
 
   def score(self, answer):
     article = wiki.get_article(answer)
@@ -56,6 +71,7 @@ class ImageRound(Round):
       'answer': {
         'article': self.article.title,
         'articles': self.articles,
+        'scoreType': 'Raw score'
       }
     }
   
@@ -75,7 +91,11 @@ class MostFrequentWordRound(Round):
   def __init__(self):
     self.article_title = ''
     self.question = ''
-    self.data = {}
+    self.data = {
+      'answer': {
+        'scoreType': 'Word count'
+      }
+    }
   
   def setup(self, article_title):
     self.article_title = article_title
