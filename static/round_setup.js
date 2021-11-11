e952d3e4073baeb65db15d2139be8d17b97ba926
Kenni Bawden <kenni.bawden@gmail.com>
2021-11-11 22:54:07 +1100

    Added feedback for when a user selects an article during round setup

--- static/round_setup.js
+++ static/round_setup.js
@@ -2,7 +2,7 @@ Vue.component('round-setup', {
   props: ['room', 'player', 'round'],
   data() {
     return {
-      playerSelection: '',
+      playerSelection: undefined,
     }
   },
   methods: {
@@ -19,11 +19,15 @@ Vue.component('round-setup', {
     <h3>Choose an article</h3>
 
     <div v-if="round.playerChoice.player == player">
-      <div class="options">
+      <div v-if="playerSelection == undefined" class="options">
         <button v-for="option in round.playerChoice.options"
           v-on:click="choose(option)">
           {{ option }}
         </button>
+      </div>
+      <div v-else>
+        You chose 
+        <em>{{playerSelection}}</em>
       </div>
     </div>
     <div v-else>
